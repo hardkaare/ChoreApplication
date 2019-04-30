8952515f56608ae27a75e5bc2702b631f6182e5d
Dillerbobs <nicklasandersen06@gmail.com>
2019-04-30 14:03:26 +0200

    ParentUI AllChores stored in new list

--- ChoreApplication/UI/ParentInterface.cs
+++ ChoreApplication/UI/ParentInterface.cs
@@ -13,10 +13,47 @@ namespace ChoreApplication.UI
     public partial class ParentInterface : Form
     {
         public int UI = 0;
+        private Dictionary<int, string> StatusValues;
+        private Dictionary<int, string> ChildrenNames;
+        private List<Chore> AllChores;
+        private readonly Font StandardFont = new Font("Microsoft Sans Serif", 10F);
         public ParentInterface()
         {
+            InitializeDictionaries();
+            LoadAllChores();
             InitializeComponent();
             ChoresUI();
+        }
+        private void LoadAllChores()
+        {
+            var ConcreteChores = Concrete.Load("");
+            var ReoccurringChores = Reocurring.Load("");
+            var RepeatableChores = Repeatable.Load("");
+
+            AllChores = new List<Chore>(ConcreteChores.Count +
+                ReoccurringChores.Count +
+                RepeatableChores.Count);
+            AllChores.AddRange(ConcreteChores);
+            AllChores.AddRange(ReoccurringChores);
+            AllChores.AddRange(RepeatableChores);
+        }
+
+        private void InitializeDictionaries()
+        {
+            StatusValues = new Dictionary<int, string>
+            {
+                { 1, "Active" },
+                { 2, "Approval pending" },
+                { 3, "Approves" },
+                { 4, "Overdue" }
+            };
+
+            ChildrenNames = new Dictionary<int, string>();
+            var Children = ChildUser.Load("");
+            foreach (var child in Children)
+            {
+                ChildrenNames.Add(child.ChildId, child.FirstName);
+            }
         }
 
         private void OptionButton_Click(object sender, EventArgs e)
@@ -75,46 +112,88 @@ namespace ChoreApplication.UI
 
         public void LoadChores()
         {
-            var ConcreteChores = Concrete.Load("");
-            var Children = ChildUser.Load("");
-            var ChildrenNames = new Dictionary<int, string>();
-
-            foreach (var child in Children)
+            int i = 0;
+            foreach (var chore in AllChores)
             {
-                ChildrenNames.Add(child.ChildId, child.FirstName);
-            }
-
-            foreach (var chore in ConcreteChores)
-            {
-                var individualChorePanel = new Panel
-                {
-                    Name = "panel" + chore.ID.ToString(),
-                    Location = new Point(10, 10),
-                    BorderStyle = BorderStyle.FixedSingle,
-                };
                 var choreName = new Label
                 {
                     Name = "choreTitle" + chore.ID.ToString(),
+                    Font = StandardFont,
                     Text = chore.Name.ToString(),
-                    Location = new Point(10, 10),
+                    Location = new Point(5, 5),
+                    AutoSize = true,
                 };
                 var choreAssignment = new Label
                 {
                     Name = "choreAssignment" + chore.ID.ToString(),
-                    Text = "Assigned to: " + ChildrenNames[chore.Assignment],
-                    Location = new Point(10, 30),
+                    Font = StandardFont,
+                    Text = "Assigned to: "
+                           + ChildrenNames[chore.Assignment],
+                    Location = new Point(10, choreName.Location.Y + 20),
+                    AutoSize = true,
                 };
                 var choreStatus = new Label
                 {
                     Name = "choreStatus" + chore.ID.ToString(),
-                    Text = "Status: " + chore.Status.ToString(),
-                    Location = new Point(10, 60),
+                    Font = StandardFont,
+                    Text = "Status: ",
+                    Location = new Point(10, choreAssignment.Location.Y + 20),
+                    AutoSize = true,
                 };
+                var panelHeight = choreName.Height + choreAssignment.Height + choreStatus.Height;
+                var individualChorePanel = new Panel
+                {
+                    Name = "panel" + chore.ID.ToString(),
+                    Location = new Point(1, i * 70),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Size = new Size(ChorePanel.Width - 5, panelHeight),
+                    AutoSize = true,
+                };
+                //if (chore.Status == 2)
+                //{
+                //    individualChorePanel.Controls.Add(AddApproveButton(individualChorePanel.Width - 45, panelHeight / 2));
+                //    individualChorePanel.Controls.Add(AddDenyButton(individualChorePanel.Width - 90, panelHeight / 2));
+                //}
                 ChorePanel.Controls.Add(individualChorePanel);
                 individualChorePanel.Controls.Add(choreName);
                 individualChorePanel.Controls.Add(choreAssignment);
                 individualChorePanel.Controls.Add(choreStatus);
+                i++;
             }
+        }
+
+        private Control AddApproveButton(int x, int y)
+        {
+            var ApproveButton = new Button
+            {
+                Location = new Point(x, y - 15),
+                Size = new Size(30, 30),
+                FlatStyle = FlatStyle.Flat,
+                BackgroundImage = global::ChoreApplication.Properties.Resources.thumbs_up,
+                BackgroundImageLayout = ImageLayout.Zoom,
+                AutoSize = true,
+            };
+            ApproveButton.FlatAppearance.BorderSize = 0;
+            ApproveButton.FlatAppearance.MouseOverBackColor = SystemColors.Window;
+
+            return ApproveButton;
+        }
+
+        private Control AddDenyButton(int x, int y)
+        {
+            var DenyButton = new Button
+            {
+                Location = new Point(x, y - 15),
+                Size = new Size(30, 30),
+                FlatStyle = FlatStyle.Flat,
+                BackgroundImage = global::ChoreApplication.Properties.Resources.thumb_down,
+                BackgroundImageLayout = ImageLayout.Zoom,
+                AutoSize = true,
+            };
+            DenyButton.FlatAppearance.BorderSize = 0;
+            DenyButton.FlatAppearance.MouseOverBackColor = SystemColors.Window;
+
+            return DenyButton;
         }
         #endregion
 
