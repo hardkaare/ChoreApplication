52baf6c9771f8811a3875270e84f31023afbaae9
Jonathan Thomas van Leeuwen <jvanle14@student.aau.dk>
2019-05-13 14:42:35 +0200

    Moved ChoreUI to chorefunctions

--- ChoreApplication/Functions/ParentFunctions/UserFunctions.cs
+++ ChoreApplication/Functions/ParentFunctions/UserFunctions.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ChoreApplication.Functions.ParentFunctions
 {
-    class UserFunctions
+    internal abstract class UserFunctions
     {
     }
 }
