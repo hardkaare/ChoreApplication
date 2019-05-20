30fda36c9890705a19e541d5dd77f57046170fba
Jonathan Thomas van Leeuwen <jvanle14@student.aau.dk>
2019-05-20 10:16:03 +0200

    Commented GeneralUI

--- ChoreApplication/UI/GeneralInterface/LoginInterface.cs
+++ ChoreApplication/UI/GeneralInterface/LoginInterface.cs
@@ -4,8 +4,20 @@ using System.Windows.Forms;
 
 namespace ChoreApplication.UI.GeneralInterface
 {
+    /// <summary>
+    /// This is the first UI in the program, the LoginInterface.
+    /// Designer adds a textbox for email and password userinput.
+    /// Designer adds button for Login
+    /// </summary>
     public partial class LoginInterface : Form
     {
+        #region Constructor
+
+        /// <summary>
+        /// Creates and displays elements in designer.
+        /// Initializes DB.
+        /// If there are no Users in DB, leads to RegisterUserInterface
+        /// </summary>
         public LoginInterface()
         {
             InitializeComponent();
@@ -13,19 +25,37 @@ namespace ChoreApplication.UI.GeneralInterface
             CheckIfUsersExist();
         }
 
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Checks if there are Users in the DB. If not opens a new RegisterUserInterface
+        /// </summary>
         private void CheckIfUsersExist()
         {
+            //True if Users in DB; otherwise false.
             bool users = true;
+
+            //Creates a query that selects all user_id's
             string query = "SELECT user_id FROM users";
             SqlCommand comd = new SqlCommand(query, Functions.DatabaseFunctions.DatabaseConnection);
+
+            //Executes the query
             Functions.DatabaseFunctions.DatabaseConnection.Open();
             SqlDataReader reader = comd.ExecuteReader();
+
+            //If there are no rows in DB users becomes false
             if (!reader.HasRows)
             {
                 users = false;
             }
+
+            //Clean up
             reader.Close();
             Functions.DatabaseFunctions.DatabaseConnection.Close();
+
+            //If there are no Users in DB a new RegisterUserInterface is created and this is hidden
             if (!users)
             {
                 var registerUserInterface = new RegisterUserInterface();
@@ -38,48 +68,76 @@ namespace ChoreApplication.UI.GeneralInterface
             }
         }
 
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Deletes text in emailTextBox when clicked.
+        /// </summary>
         private void EmailTextbox_Click(object sender, EventArgs e)
         {
-            //Få tekstbokse til at slette indhold ved første selection.
             emailTextBox.Text = "";
         }
 
+        /// <summary>
+        /// Deletes text in passwordTextBox when clicked
+        /// </summary>
         private void PasswordTextBox_Click(object sender, EventArgs e)
         {
             passwordTextBox.Text = "";
-            passwordTextBox.UseSystemPasswordChar = true;
         }
 
+        /// <summary>
+        /// Validates user input and logs in if correct
+        /// </summary>
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            //Pulls user input
             string emailInput = emailTextBox.Text;
             string passwordInput = passwordTextBox.Text;
+
+            //True if user input match DB
             bool isMatch = false;
+
+            //Displays error message if no input in one or more textbox
             if (string.IsNullOrEmpty(emailInput) || string.IsNullOrEmpty(passwordInput))
             {
                 MessageBox.Show("Please enter your E-mail and password.", "Error");
                 return;
             }
 
+            //Creates  query that selects email and password from DB
             string loginQuery = "SELECT email, password FROM dbo.parent";
             SqlCommand command = new SqlCommand(loginQuery, Functions.DatabaseFunctions.DatabaseConnection);
+
+            //Executes the query
             Functions.DatabaseFunctions.DatabaseConnection.Open();
             SqlDataReader reader = command.ExecuteReader();
+
+            //Reads loaded data from DB
             while (reader.Read())
             {
+                //Sets email and password from DB
                 string email = reader["email"].ToString();
                 string password = reader["password"].ToString();
+
+                //Checks if input matches DB and sets isMatch to true if so
                 if (emailInput == email && passwordInput == password)
                 {
                     isMatch = true;
                 }
+
+                //Displays error message if no match
                 else
                 {
-                    MessageBox.Show("Incorrect password or E-mail entered.");
+                    MessageBox.Show("Incorrect password or E-mail entered.", "Error");
                     Functions.DatabaseFunctions.DatabaseConnection.Close();
                     return;
                 }
             }
+
+            //If user input and DB match proceeds to ChooseProfileInterface
             if (isMatch == true)
             {
                 Functions.DatabaseFunctions.DatabaseConnection.Close();
@@ -88,5 +146,7 @@ namespace ChoreApplication.UI.GeneralInterface
                 this.Hide();
             }
         }
+
+        #endregion
     }
 }
--- ChoreApplication/UI/GeneralInterface/RegisterUserInterface.cs
+++ ChoreApplication/UI/GeneralInterface/RegisterUserInterface.cs
@@ -4,44 +4,62 @@ using System.Windows.Forms;
 
 namespace ChoreApplication.UI.GeneralInterface
 {
+    /// <summary>
+    /// Displays the RegisterUserInterface that creates a ParentUser in the DB. 
+    /// Designer adds textboxes for all userinput.
+    /// Designer adds a button for Register user.
+    /// This class contains event handlers for Register user button.
+    /// </summary>
     public partial class RegisterUserInterface : Form
     {
+        #region Constructor 
+
+        /// <summary>
+        /// Creates and displays elements in the designer
+        /// </summary>
         public RegisterUserInterface()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Event Handlers
+
         private void RegisterUserButton_Click(object sender, EventArgs e)
         {
             try
             {
-                //Validate first name
+                //Validate first name. Cannot be longer than 20 chars and can only contain chars from the danish alphabet
                 if ((firstNameTextBox.Text.Length < 20) && Regex.IsMatch(firstNameTextBox.Text, @"^[ÆØÅæøåa-zA-Z]+$"))
                 {
                     string firstName = firstNameTextBox.Text;
 
-                    //Validate last name
+                    //Validate last name. Cannot be longer than 50 chars and can only contain chars from the danish alphabet
                     if ((lastNameTextBox.Text.Length < 50) && Regex.IsMatch(lastNameTextBox.Text, @"^[ÆØÅæøåa-zA-Z\s]+$"))
                     {
                         string lastName = lastNameTextBox.Text;
 
-                        //Validate email
+                        //Validate email. Cannot be longer than 50 chars. 
+                        //Must contain in order any number of chars, a @, any number of chars, a dot, and any number of chars
                         if ((emailTextBox.Text.Length < 50) && Regex.IsMatch(emailTextBox.Text, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
                         {
                             string email = emailTextBox.Text;
 
-                            //Validate password text
+                            //Validate password text. Cannot be longer than 20 chars. 
                             if ((password1TextBox.Text.Length < 20) && Regex.IsMatch(password1TextBox.Text, @"^[ÆØÅæøåa-zA-Z0-9]+$") && (password1TextBox.Text.Length >= 6))
                             {
                                 string password = password1TextBox.Text;
 
-                                //Validate password2
+                                //Checks that passwords match
                                 if (password1TextBox.Text == password2TextBox.Text)
                                 {
-                                    //Validate pincode
+                                    //Validate pincode. Cannot be longer than 4 chars and must only contain numbers
                                     if (Regex.IsMatch(pincodeTextBox.Text, @"^\d{4}$"))
                                     {
                                         string pincode = pincodeTextBox.Text;
+
+                                        //Inserts a ParentUser from user input and proceeds to loginInterface
                                         Model.ParentUser.Insert(firstName, lastName, email, password, pincode);
                                         var loginInterface = new LoginInterface();
                                         loginInterface.Show();
@@ -82,5 +100,7 @@ namespace ChoreApplication.UI.GeneralInterface
                 MessageBox.Show("An unexpected error occurred. Please restart the program", "Error");
             }
         }
+
+        #endregion
     }
 }
