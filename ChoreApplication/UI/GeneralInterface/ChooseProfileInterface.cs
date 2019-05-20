c7eae5db0853bd9be644061cdfdc796fb18f9b2f
Jonathan Thomas van Leeuwen <jvanle14@student.aau.dk>
2019-05-20 10:16:03 +0200

    Commented GeneralUI

--- ChoreApplication/UI/GeneralInterface/ChooseProfileInterface.cs
+++ ChoreApplication/UI/GeneralInterface/ChooseProfileInterface.cs
@@ -5,65 +5,118 @@ using System.Windows.Forms;
 
 namespace ChoreApplication.UI.GeneralInterface
 {
+    /// <summary>
+    /// Displays a form with where the User can choose which profile to use. Leads to PincodeInterface.
+    /// Adds a button on runtime for each User in the DB.
+    /// Contains event handlers UserButton_Click and BackButton_Click.
+    /// </summary>
     public partial class ChooseProfileInterface : Form
     {
-        public static int ActiveID;
+        #region Properties
 
+        //Surname of the family
         private string Surname { get; set; }
 
+        #endregion
+
+        #region Constructor
+
+        //Displays component created by the designer
         public ChooseProfileInterface()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Creates and displays a profilebutton for each User. Is executed when the form is loaded.
+        /// </summary>
         private void ChooseProfile_Load(object sender, EventArgs e)
         {
+            //Creates a query to load last_name from DB
             string query = "SELECT last_name FROM parent";
+
+            //Executes the query
             Functions.DatabaseFunctions.DatabaseConnection.Open();
             SqlCommand command = new SqlCommand(query, Functions.DatabaseFunctions.DatabaseConnection);
             SqlDataReader reader = command.ExecuteReader();
+
+            //Reads the line and sets Surname
             while (reader.Read())
             {
                 Surname = reader["last_name"].ToString();
             }
             Functions.DatabaseFunctions.DatabaseConnection.Close();
+
+            //Displays Surname
             surnameLabel.Text = "The " + Surname + "'s";
 
+            //Loads a list of all ParentUsers and ChildUsers
             var parentUsers = Model.ParentUser.Load("");
             var childUsers = Model.ChildUser.Load("");
 
+            //Sets ints for relative locations in 2*x grit format. LocationCounter1 is the nr. for x location and locationCounter2 is for y location
             int locationCounter1 = 1;
             int locationCounter2 = 1;
+
+            //Displays name and a button for each ParentUser
             foreach (Model.ParentUser parent in parentUsers)
             {
+                //Creates a standard button from UILibrary with ID as tag. 
                 Button userButton = UILibrary.StandardElements.AddImageButton(new Point(locationCounter2 * 120, locationCounter1 * 100 - 90), parent.ID, global::ChoreApplication.Properties.Resources.user);
+
+                //Resizes and adds even handler
                 userButton.Size = new Size(60, 60);
                 userButton.Click += new EventHandler(UserButton_Click);
+
+                //Creates a standard label with the User's first name
                 var nameLabel = UILibrary.StandardElements.AddLabel(parent.FirstName, true, new Point(userButton.Location.X - 7, userButton.Location.Y + userButton.Height));
+
+                //Adds the button and label to UI
                 profilesPanel.Controls.Add(userButton);
                 profilesPanel.Controls.Add(nameLabel);
+
+                //If last button/label was displayed to the left, display the next to the right
                 if (locationCounter2 == 1)
                 {
                     locationCounter2 = 2;
                 }
+
+                //If last button/label was displayed to the right, display the next to the left and a row below
                 else if (locationCounter2 == 2)
                 {
                     locationCounter2 = 1;
                     locationCounter1++;
                 }
             }
+
+            //Displays a name and button for each ChildUser
             foreach (Model.ChildUser child in childUsers)
             {
+                //Creates a standard button from UILibrary with ID as tag. 
                 Button userButton = UILibrary.StandardElements.AddImageButton(new Point(locationCounter2 * 120, locationCounter1 * 100 - 90), child.ID, global::ChoreApplication.Properties.Resources.user);
+
+                //Resizes and adds even handler
                 userButton.Size = new Size(60, 60);
                 userButton.Click += new EventHandler(UserButton_Click);
+
+                //Creates a standard label with the User's first name
                 var nameLabel = UILibrary.StandardElements.AddLabel(child.FirstName, false, new Point(userButton.Location.X - 7, userButton.Location.Y + userButton.Height));
+
+                //Adds the button and label to UI
                 profilesPanel.Controls.Add(userButton);
                 profilesPanel.Controls.Add(nameLabel);
+
+                //If last button/label was displayed to the left, display the next to the right
                 if (locationCounter2 == 1)
                 {
                     locationCounter2 = 2;
                 }
+
+                //If last button/label was displayed to the right, display the next to the left and a row below
                 else if (locationCounter2 == 2)
                 {
                     locationCounter2 = 1;
@@ -72,20 +125,27 @@ namespace ChoreApplication.UI.GeneralInterface
             }
         }
 
+        //Creates a new PincodeInterface, passing ID from the button's tag
         private void UserButton_Click(object sender, System.EventArgs e)
         {
+            //Sets ActiveID to the clicked button's tag. 
             Button clickedButton = (Button)sender;
-            ActiveID = (int)clickedButton.Tag;
-            var pinCodeUI = new PinCodeInterface();
+            int ActiveID = (int)clickedButton.Tag;
+            
+            //Creates a new PincodeInterface and closes this one
+            var pinCodeUI = new PinCodeInterface(ActiveID);
             pinCodeUI.Show();
             this.Close();
         }
 
+        //Returns to the previous interface
         private void BackButton_Click(object sender, EventArgs e)
         {
             var loginUI = new LoginInterface();
             loginUI.Show();
             this.Close();
         }
+
+        #endregion
     }
 }
--- ChoreApplication/UI/GeneralInterface/PinCodeInterface.cs
+++ ChoreApplication/UI/GeneralInterface/PinCodeInterface.cs
@@ -4,72 +4,129 @@ using System.Windows.Forms;
 
 namespace ChoreApplication.UI.GeneralInterface
 {
+    /// <summary>
+    /// Displays the PincodeInterface. 
+    /// Textbox for user input is added in the designer.
+    /// Round buttons for numbers 0-9 are added in the designer, as well as accept and delete button.
+    /// This class contains event handlers for each numeric button, accept button, delete button and back button.
+    /// </summary>
     public partial class PinCodeInterface : Form
     {
-        private Model.User _session;
+        #region Properties
 
-        public PinCodeInterface()
+        //ID for the chosen User in ChooseProfile
+        private int ActiveID { get; set; }
+
+        //Session that's passed to ParentMenu or ChildMenu on login
+        private Model.User _session { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes components from designer. Sets ActiveID
+        /// </summary>
+        public PinCodeInterface(int id)
         {
             InitializeComponent();
+            ActiveID = id;
         }
 
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Event handler for the numeric buttons. Adds the number to enterpinTextBox's text
+        /// </summary>
         private void OneButton_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             enterpinTextBox.Text += button.Text;
         }
 
+        /// <summary>
+        /// Event handler for DeleteButton. Clears enterpinTextBox.Text
+        /// </summary>
         private void DeleteButton_Click(object sender, EventArgs e)
         {
             enterpinTextBox.Text = "";
         }
 
+        /// <summary>
+        /// Event handler for AcceptButton. Validates userinput and sends to respective menu.
+        /// </summary>
         private void AcceptButton_click(object sender, EventArgs e)
         {
+            //True if pincode was correct
             bool correctpin = false;
+
+            //Is true if enterpinTextBox.Text is converted to int 
             bool conversion = Int32.TryParse(enterpinTextBox.Text, out int pincode);
 
+            //If conversion is true checks if pincode matches the chosen User's pincode
             if (conversion)
             {
-                string query = string.Format("SELECT pincode FROM users WHERE user_id={0}", ChooseProfileInterface.ActiveID);
+                //Creates a query that selects pincode from chosen User
+                string query = string.Format("SELECT pincode FROM users WHERE user_id={0}", ActiveID);
                 Functions.DatabaseFunctions.DatabaseConnection.Open();
 
-                //Creates the SqlCommand and executes it
+                //Executes the query
                 SqlCommand cmd = new SqlCommand(query, Functions.DatabaseFunctions.DatabaseConnection);
                 SqlDataReader reader = cmd.ExecuteReader();
 
+                //Reads the pincode
                 while (reader.Read())
                 {
-                    int enteredpin = Convert.ToInt32(reader["pincode"]);
-                    if (enteredpin == pincode)
+                    //Converts pin from DB to int
+                    int loadedpin = Convert.ToInt32(reader["pincode"]);
+
+                    //If the pins match correctpin becomes true
+                    if (loadedpin == pincode)
                     {
                         correctpin = true;
                     }
+
+                    //Displays error message if incorrect pin
                     else
                     {
-                        MessageBox.Show("Incorrect pincode");
+                        MessageBox.Show("Incorrect pincode", "Error");
                     }
                 }
             }
+
+            //Displays error message if text could not be converted to int
             else
             {
-                MessageBox.Show("Please enter numbers in your pincode");
+                MessageBox.Show("Please enter numbers in your pincode", "Error");
             }
             Functions.DatabaseFunctions.DatabaseConnection.Close();
+
+            //If pins match logs into respective interface
             if (correctpin == true)
             {
-                if (ChooseProfileInterface.ActiveID == 1)
+                //If chosen profile was the parent, logs into ParentMenu
+                if (ActiveID == 1)
                 {
+                    //Loads ParentUser and sets _session
                     var sessionList = Model.ParentUser.Load("");
                     _session = sessionList[0];
+
+                    //Creates ParentMenu with _session and closes this
                     var parentUI = new ParentUI.ParentMenu(sessionList[0]);
                     parentUI.Show();
                     this.Close();
                 }
+
+                //If chosen profile was a child, logs into ChildMenu
                 else
                 {
-                    var sessionList = Model.ChildUser.Load("u.user_id=" + ChooseProfileInterface.ActiveID.ToString());
+                    //Loads ChildUser with the chosen ID and sets _session
+                    var sessionList = Model.ChildUser.Load("u.user_id=" + ActiveID.ToString());
                     _session = sessionList[0];
+
+                    //Creates ChildMenu with _session and closes this
                     var childUI = new UI.ChildMenu(sessionList[0]);
                     childUI.Show();
                     this.Close();
@@ -77,11 +134,16 @@ namespace ChoreApplication.UI.GeneralInterface
             }
         }
 
+        /// <summary>
+        /// Event handler for BackButton. Returns to previous UI
+        /// </summary>
         private void BackButton_Click(object sender, EventArgs e)
         {
             var chooseProfile = new ChooseProfileInterface();
             chooseProfile.Show();
             this.Close();
         }
+
+        #endregion
     }
 }
