a568ce81d05d8586a023c4132ff63bccd73e101c
Nicklas Kaare <nicklasandersen06@gmail.com>
2019-05-21 15:12:58 +0200

    Fix

---
+++ ChoreApplication/TestForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿﻿namespace ChoreApplication
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.resetDatabaseButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dummyDataButton = new System.Windows.Forms.Button();
+            this.applicationOptionsGroupBox = new System.Windows.Forms.GroupBox();
+            this.openApplicationButton = new System.Windows.Forms.Button();
+            this.checkTimeButton = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.applicationOptionsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // resetDatabaseButton
+            // 
+            this.resetDatabaseButton.Location = new System.Drawing.Point(6, 19);
+            this.resetDatabaseButton.Name = "resetDatabaseButton";
+            this.resetDatabaseButton.Size = new System.Drawing.Size(109, 41);
+            this.resetDatabaseButton.TabIndex = 0;
+            this.resetDatabaseButton.Text = "Clear Database";
+            this.toolTip.SetToolTip(this.resetDatabaseButton, "Clears the database, allowing you to register a completely new user if needed.");
+            this.resetDatabaseButton.UseVisualStyleBackColor = true;
+            this.resetDatabaseButton.Click += new System.EventHandler(this.ResetDatabaseButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dummyDataButton);
+            this.groupBox1.Controls.Add(this.resetDatabaseButton);
+            this.groupBox1.Location = new System.Drawing.Point(18, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 70);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Database Options";
+            // 
+            // dummyDataButton
+            // 
+            this.dummyDataButton.Location = new System.Drawing.Point(121, 20);
+            this.dummyDataButton.Name = "dummyDataButton";
+            this.dummyDataButton.Size = new System.Drawing.Size(109, 41);
+            this.dummyDataButton.TabIndex = 0;
+            this.dummyDataButton.Text = "Insert Dummy Data";
+            this.toolTip.SetToolTip(this.dummyDataButton, "Inserts dummy data into the database.\r\n\r\nEmail: test@test.dk\r\nPassword: 123456\r\nA" +
+        "ll pincodes: 1234");
+            this.dummyDataButton.UseVisualStyleBackColor = true;
+            this.dummyDataButton.Click += new System.EventHandler(this.DummyDataButton_Click);
+            // 
+            // applicationOptionsGroupBox
+            // 
+            this.applicationOptionsGroupBox.Controls.Add(this.openApplicationButton);
+            this.applicationOptionsGroupBox.Controls.Add(this.checkTimeButton);
+            this.applicationOptionsGroupBox.Location = new System.Drawing.Point(18, 12);
+            this.applicationOptionsGroupBox.Name = "applicationOptionsGroupBox";
+            this.applicationOptionsGroupBox.Size = new System.Drawing.Size(248, 71);
+            this.applicationOptionsGroupBox.TabIndex = 2;
+            this.applicationOptionsGroupBox.TabStop = false;
+            this.applicationOptionsGroupBox.Text = "Application Options";
+            // 
+            // openApplicationButton
+            // 
+            this.openApplicationButton.Location = new System.Drawing.Point(6, 19);
+            this.openApplicationButton.Name = "openApplicationButton";
+            this.openApplicationButton.Size = new System.Drawing.Size(109, 41);
+            this.openApplicationButton.TabIndex = 0;
+            this.openApplicationButton.Text = "Open ChoreApplication";
+            this.toolTip.SetToolTip(this.openApplicationButton, "Opens the Chore Application.");
+            this.openApplicationButton.UseVisualStyleBackColor = true;
+            this.openApplicationButton.Click += new System.EventHandler(this.OpenApplicationButton_Click);
+            // 
+            // checkTimeButton
+            // 
+            this.checkTimeButton.Location = new System.Drawing.Point(121, 19);
+            this.checkTimeButton.Name = "checkTimeButton";
+            this.checkTimeButton.Size = new System.Drawing.Size(109, 41);
+            this.checkTimeButton.TabIndex = 0;
+            this.checkTimeButton.Text = "Test CheckTime";
+            this.toolTip.SetToolTip(this.checkTimeButton, "Executes the CheckTime function.");
+            this.checkTimeButton.UseVisualStyleBackColor = true;
+            this.checkTimeButton.Click += new System.EventHandler(this.CheckTimeButton_Click);
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 166);
+            this.Controls.Add(this.applicationOptionsGroupBox);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Testing Form";
+            this.groupBox1.ResumeLayout(false);
+            this.applicationOptionsGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resetDatabaseButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button dummyDataButton;
+        private System.Windows.Forms.GroupBox applicationOptionsGroupBox;
+        private System.Windows.Forms.Button openApplicationButton;
+        private System.Windows.Forms.Button checkTimeButton;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
---
+++ ChoreApplication/TestForm.cs
@@ -0,0 +1,52 @@
+﻿﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ChoreApplication
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenApplicationButton_Click(object sender, EventArgs e)
+        {
+            _ = new UI.GeneralInterface.LoginInterface();
+        }
+
+        private void CheckTimeButton_Click(object sender, EventArgs e)
+        {
+            _ = new TechnicalPlatform.CheckTime();
+            MessageBox.Show("CheckTime function has run succesfully.\nRefresh the application to see the changes.", "Success");
+        }
+
+        private void ResetDatabaseButton_Click(object sender, EventArgs e)
+        {
+            ResetDB();
+            MessageBox.Show("The database has been cleared.", "Success");
+        }
+
+        private void DummyDataButton_Click(object sender, EventArgs e)
+        {
+            ResetDB();
+            var query = string.Format("INSERT INTO dbo.users VALUES ('Far', '1234'); INSERT INTO dbo.parent VALUES (1, 'Test', 'test@test.dk', '123456'); INSERT INTO dbo.users VALUES ('Erik', '1234'); INSERT INTO dbo.child VALUES (2, 0); INSERT INTO dbo.users VALUES ('Valdemar', '1234'); INSERT INTO dbo.child VALUES (3, 0); INSERT INTO dbo.users VALUES ('Peter', '1234'); INSERT INTO dbo.child VALUES (4, 0); INSERT INTO dbo.chore VALUES (1,'Gå med hunden', 'Husk poser til afføring',15); INSERT INTO dbo.repeatable_chore VALUES(1,3,0); INSERT INTO dbo.chore VALUES (1,'Tøm opvaskemaskinen.', 'Husk at tørre vandet ovenpå kopperne af.',10); INSERT INTO dbo.concrete_chore VALUES(2,'15-05-2019 17:00',1,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (1,'Rengør værelse', 'Tørre støv af og støvsuge',25); INSERT INTO dbo.concrete_chore VALUES(3,'15-04-2019 17:00',1,NULL,'reoc', 0); INSERT INTO dbo.chore VALUES (1,'Slå græsset.','Huske at komme godt ind til kanterne.',10); INSERT INTO dbo.concrete_chore VALUES(4,'15-05-2019 18:00',1,NULL,'conc', 0); INSERT INTO dbo.reward VALUES (1,'Fredagsslik.','200g.',75); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(5,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(6,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(7,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(8,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(9,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(10,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(11,'15-04-2019 17:00',4,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (2,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(12,'15-04-2019 17:00',4,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',70); INSERT INTO dbo.concrete_chore VALUES(13,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(14,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(15,'15-04-2019 17:00',4,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(16,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(17,'15-04-2019 17:00',4,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(18,'15-04-2019 17:00',3,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(19,'15-04-2019 17:00',4,NULL,'conc', 0); INSERT INTO dbo.chore VALUES (3,'Rengør værelse', 'Tørre støv af og støvsuge',10); INSERT INTO dbo.concrete_chore VALUES(20,'15-04-2019 17:00',4,NULL,'conc', 0);");
+            SqlCommand command = new SqlCommand(query, Functions.DatabaseFunctions.DatabaseConnection);
+            Functions.DatabaseFunctions.DatabaseConnection.Open();
+            command.ExecuteNonQuery();
+            Functions.DatabaseFunctions.DatabaseConnection.Close();
+
+            MessageBox.Show("The dummy data has been inserted.", "Success");
+        }
+
+        private void ResetDB()
+        {
+            var query = string.Format("SET DATEFORMAT dmy; IF OBJECT_ID ('dbo.days', 'U') IS NOT NULL DROP TABLE dbo.days; IF OBJECT_ID ('dbo.reoccurring_chore', 'U') IS NOT NULL DROP TABLE dbo.reoccurring_chore; IF OBJECT_ID('dbo.repeatable_chore', 'U') IS NOT NULL DROP TABLE dbo.repeatable_chore; IF OBJECT_ID('dbo.concrete_chore', 'U') IS NOT NULL DROP TABLE dbo.concrete_chore; IF OBJECT_ID('dbo.chore', 'U') IS NOT NULL DROP TABLE dbo.chore; IF OBJECT_ID ('dbo.reward', 'U') IS NOT NULL DROP TABLE dbo.reward; IF OBJECT_ID ('dbo.notification', 'U') IS NOT NULL DROP TABLE dbo.notification; IF OBJECT_ID ('dbo.child', 'U') IS NOT NULL DROP TABLE dbo.child; IF OBJECT_ID('dbo.parent', 'U') IS NOT NULL DROP TABLE dbo.parent; IF OBJECT_ID('dbo.users', 'U') IS NOT NULL DROP TABLE dbo.users; IF OBJECT_ID('dbo.checkTime', 'U') IS NOT NULL DROP TABLE dbo.checkTime; CREATE TABLE dbo.users(user_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY, first_name  VARCHAR(20) NOT NULL, pincode VARCHAR(4)); CREATE TABLE dbo.parent(user_id INT NOT NULL, last_name VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(20) NOT NULL, FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE); CREATE TABLE dbo.child(child_id    INT NOT NULL IDENTITY(1,1) PRIMARY KEY, user_id INT NOT NULL, points INT NOT NULL DEFAULT 0, FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE );    CREATE TABLE dbo.notification( notification_id     INT NOT NULL PRIMARY KEY IDENTITY(1,1), user_id             INT NOT NULL, title               VARCHAR(255) NOT NULL, description         VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE );  CREATE TABLE dbo.checkTime( dailyTick  VARCHAR(19) );   CREATE TABLE dbo.reward( reward_id   INT NOT NULL IDENTITY(1,1) PRIMARY KEY, child_id    INT NOT NULL, name        VARCHAR(50) NOT NULL, description VARCHAR(255), points      INT NOT NULL, FOREIGN KEY (child_id) REFERENCES child(child_id) ON DELETE CASCADE );  CREATE TABLE dbo.chore( chore_id    INT NOT NULL IDENTITY(1,1) PRIMARY KEY, child_id    INT NOT NULL, name        VARCHAR(50) NOT NULL, description VARCHAR(255), points      INT NOT NULL, FOREIGN KEY (child_id) REFERENCES child(child_id) ON DELETE CASCADE );  CREATE TABLE dbo.concrete_chore( chore_id        INT NOT NULL, due_date        VARCHAR(19), status          INT NOT NULL, approval_date   VARCHAR(19), type            VARCHAR(11) NOT NULL, reminder        INT NOT NULL, FOREIGN KEY (chore_id) REFERENCES chore(chore_id) ON DELETE CASCADE );  CREATE TABLE dbo.repeatable_chore( chore_id        INT NOT NULL, limit           INT, completions     INT NOT NULL, FOREIGN KEY (chore_id) REFERENCES chore(chore_id) ON DELETE CASCADE ); CREATE TABLE dbo.reoccurring_chore( reo_id      INT NOT NULL PRIMARY KEY IDENTITY(1,1), chore_id    INT NOT NULL, due_time    VARCHAR(19) NOT NULL, FOREIGN KEY (chore_id) REFERENCES chore(chore_id) ON DELETE CASCADE );  CREATE TABLE dbo.days( reo_id  INT NOT NULL, day     CHAR(9), FOREIGN KEY (reo_id) REFERENCES reoccurring_chore(reo_id) ON DELETE CASCADE );");
+            SqlCommand command = new SqlCommand(query, Functions.DatabaseFunctions.DatabaseConnection);
+            Functions.DatabaseFunctions.DatabaseConnection.Open();
+            command.ExecuteNonQuery();
+            Functions.DatabaseFunctions.DatabaseConnection.Close();
+        }
+    }
+}
