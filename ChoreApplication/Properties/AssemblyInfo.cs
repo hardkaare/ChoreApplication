4780ad0261f27bd68a56b15e0b093b5f363b7832
Nicklas Kaare <nicklasandersen06@gmail.com>
2019-05-21 15:24:12 +0200

    Fix

--- ChoreApplication/Properties/AssemblyInfo.cs
+++ ChoreApplication/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChoreApplication")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Chore Application")]
+[assembly: AssemblyDescription("System to handle chores within a household.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChoreApplication")]
+[assembly: AssemblyCompany("BI408F19")]
+[assembly: AssemblyProduct("Chore Application")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-DK")]
+
